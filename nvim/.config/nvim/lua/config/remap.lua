c34c503b4702b0f2930751a4923ffd8290eb08ba
Dominic Galiano <dgaliano@utexas.edu>
2024-08-02 14:30:21 -0400

    Add code action preview plugin

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -56,7 +56,9 @@ vim.keymap.set('n', '<leader>wl', function()
 end, opts)
 vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+-- Don't need this any more now that I am using actions-preview.nvim for
+-- a more rich code action preview
+-- vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 vim.keymap.set('n', '<leader>f', function()
     vim.lsp.buf.format({ async = true })
